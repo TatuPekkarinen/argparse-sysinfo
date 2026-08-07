ee3f65f36af8f5ca8cde24be7d8e62a6c6ad0a9c
TTP4 <tatupek7@gmail.com>
2026-08-07 23:16:42 +0300

    code cleanup

--- src/main.py
+++ src/main.py
@@ -25,16 +25,15 @@ def main():
     match args.subarg:
         case "core": 
             cpuinf_path = os.path.join(os.path.dirname(__file__), "exec", "cpuinf.exe")
-            subprocess.run([f"{cpuinf_path}"], shell=True)
+            subprocess.run([cpuinf_path], shell=True)
             for core_num, percentage in enumerate(psutil.cpu_percent(interval=1, percpu=True)):
                 print(f"Core {core_num}: {percentage}%")
             frequency = psutil.cpu_freq(percpu=False)
-            freq_current = frequency.current
-            print(f"Current Frequency: {freq_current} MHz")
+            print(f"Estimate frequency: {frequency.current} MHz")
         case "mem":
             memory = psutil.virtual_memory()
-            print(f"Total memory: {byte_to_gig(memory.total):2f} GB")
-            print(f"Memory used: {byte_to_gig(memory.used):2f} GB")
+            print(f"Total memory: {byte_to_gig(memory.total):2f} GiB")
+            print(f"Memory used: {byte_to_gig(memory.used):2f} GiB")
         case "version":
             print(f"Current version: ({ParamInfo.version})")
         case _:
